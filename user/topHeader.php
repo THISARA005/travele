77941fe44c63d28b9aa444b68879aa2b643729f9
B.A Thisara Dilshan <88025945+THISARA005@users.noreply.github.com>
2023-07-19 18:57:14 +0530

    create the user folder and start to edit the frontend.

---
+++ user/topHeader.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- favicon -->
+    <link rel="icon" type="image/png" href="assets/images/favicon.png">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/vendors/bootstrap/css/bootstrap.min.css" media="all">
+    <!-- Fonts Awesome CSS -->
+    <link rel="stylesheet" type="text/css" href="assets/vendors/fontawesome/css/all.min.css">
+    <!-- jquery-ui css -->
+    <link rel="stylesheet" type="text/css" href="assets/vendors/jquery-ui/jquery-ui.min.css">
+    <!-- modal video css -->
+    <link rel="stylesheet" type="text/css" href="assets/vendors/modal-video/modal-video.min.css">
+    <!-- light box css -->
+    <link rel="stylesheet" type="text/css" href="assets/vendors/lightbox/dist/css/lightbox.min.css">
+    <!-- slick slider css -->
+    <link rel="stylesheet" type="text/css" href="assets/vendors/slick/slick.css">
+    <link rel="stylesheet" type="text/css" href="assets/vendors/slick/slick-theme.css">
+    <!-- google fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,400&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,400&display=swap" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<header id="masthead" class="site-header header-primary">
+            <!-- header html start -->
+            <div class="top-header">
+               <div class="container">
+                  <div class="row">
+                     <div class="col-lg-8 d-none d-lg-block">
+                        <div class="header-contact-info">
+                           <ul>
+                              <li>
+                                 <a href="#"><i class="fas fa-phone-alt"></i> +94 76 130 2240</a>
+                              </li>
+                              <li>
+                                 <a href="mailto:info@Travel.com"><i class="fas fa-envelope"></i> company@domain.com</a>
+                              </li>
+                              <li>
+                                 <i class="fas fa-map-marker-alt"></i> 76/2, Nugegoda, Sri Lanka
+                              </li>
+                           </ul>
+                        </div>
+                     </div>
+                     <div class="col-lg-4 d-flex justify-content-lg-end justify-content-between">
+                        <div class="header-social social-links">
+                           <ul>
+                              <li><a href="#"><i class="fab fa-facebook-f" aria-hidden="true"></i></a></li>
+                              <li><a href="#"><i class="fab fa-twitter" aria-hidden="true"></i></a></li>
+                              <li><a href="#"><i class="fab fa-instagram" aria-hidden="true"></i></a></li>
+                              <li><a href="#"><i class="fab fa-linkedin" aria-hidden="true"></i></a></li>
+                           </ul>
+                        </div>
+                        <div class="header-search-icon">
+                           <button class="search-icon">
+                              <i class="fas fa-search"></i>
+                           </button>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="bottom-header">
+               <div class="container d-flex justify-content-between align-items-center">
+                  <div class="site-identity">
+                     <p class="site-title">
+                        <a href="index.html">
+                           <img class="white-logo" src="assets/images/travele-logo.png" alt="logo">
+                           <img class="black-logo" src="assets/images/travele-logo1.png" alt="logo">
+                        </a>
+                     </p>
+                  </div>
+                  <div class="main-navigation d-none d-lg-block">
+                     <nav id="navigation" class="navigation">
+                        <ul>
+                           <li class="menu-item-has-children">
+                              <a href="index.php">Home</a>
+                              <!--
+                              <ul>
+                                 <li>
+                                    <a href="index-v2.html">Home 2</a>
+                                 </li>
+                              </ul>-->
+                           </li>
+                           <li class="menu-item-has-children">
+                              <a href="#">Tour</a>
+                              <ul>
+                              <li>
+                                    <a href="gallery.html">Gallery</a>
+                                 </li>
+                                 <li>
+                                    <a href="destination.php">Destination</a>
+                                 </li>
+                                 <li>
+                                    <a href="tour-packages.html">Tour Packages</a>
+                                 </li>
+                                 <li>
+                                    <a href="package-offer.html">Package Offer</a>
+                                 </li>
+                                 <li>
+                                    <a href="package-detail.html">Package Detail</a>
+                                 </li>
+                                 <li>
+                                    <a href="tour-cart.html">Tour Cart</a>
+                                 </li>
+                                 <li>
+                                    <a href="booking.html">Package Booking</a>
+                                 </li>
+                                 <li>
+                                    <a href="confirmation.html">Confirmation</a>
+                                 </li>
+                              </ul>
+                           </li>
+                             <!--<li class="menu-item-has-children">
+                             <a href="#">Pages</a>
+                              <ul> 
+                                 <li>
+                                    <a href="single-page.html">Single Page</a>
+                                 </li>
+                                 <li>
+                                    <a href="popup.html">Popup</a>
+                                 </li>
+                                 <li>
+                                    <a href="search-page.html">Search Page</a>
+                                 </li>
+                                 <li>
+                                    <a href="404.html">404 Page</a>
+                                 </li>
+                                 <li>
+                                    <a href="comming-soon.html">Comming Soon</a>
+                                 </li>
+                                 
+                                 <li>
+                                    <a href="wishlist-page.html">Wishlist</a>
+                                 </li>
+                              </ul>
+                           </li>-->
+                           <li class="menu-item-has-children">
+                              <a href="#">Join With Us</a>
+                              <ul>
+                                 <li>
+                                    <a href="career.html">Career</a>
+                                 </li>
+                                 <li>
+                                    <a href="career-detail.html">Career Detail</a>
+                                 </li>
+                                 <li>
+                                    <a href="tour-guide.html">Tour Guide</a>
+                                 </li>
+                              </ul>
+                           </li>
+                           <li class="menu-item-has-children">
+                              <a href="single-page.html">Shop</a>
+                              <ul>
+                                 <li>
+                                    <a href="product-right.html">Shop Archive</a>
+                                 </li>
+                                 <li>
+                                    <a href="product-detail.html">Shop Single</a>
+                                 </li>
+                                 <li>
+                                    <a href="product-cart.html">Shop Cart</a>
+                                 </li>
+                                 <li>
+                                    <a href="product-checkout.html">Shop Checkout</a>
+                                 </li>
+                              </ul>
+                           </li>
+                           <li class="menu-item-has-children">
+                              <a href="#">Blog</a>
+                              <ul>
+
+                                 <li><a href="blog-archive.html">Blog List</a></li>
+                                 <!--<li><a href="blog-archive-left.html">Blog Left Sidebar</a></li>
+                                 <li><a href="blog-archive-both.html">Blog Both Sidebar</a></li>
+                                 <li><a href="blog-single.html">Blog Single</a></li>-->
+                                 <li>
+                                    <a href="testimonial-page.html">Famous Posts</a>
+                                 </li>
+                              </ul>
+                           </li>
+                           <li class="menu-item-has-children">
+                              <a href="about.html">About us</a>
+                              <ul>
+                              
+                                 <li>
+                                    <a href="service.html">Our Services</a>
+                                 </li>
+                                 <li>
+                                    <a href="contact.html">Contact us</a>
+                                 </li>
+                                 <li>
+                                    <a href="faq.html">FAQ's </a>
+                                 </li>
+                              </ul>
+                           </li>
+                           
+                        </ul>
+                     </nav>
+                  </div>
+                  <div class="header-btn">
+                     <a href="normal_user_registration.html" class="button-primary">Register Now</a>
+                  </div>
+                  <div class="header-btn">
+                     <a href="login.html" class="button-primary" >Login</a>
+                  </div>
+               </div>
+            </div>
+            <div class="mobile-menu-container"></div>
+         </header>
+         
+   <!-- Add any JavaScript or script links here -->
+   <script src="assets/js/jquery.js"></script>
+      <script src="http://cdnjs.cloudflare.com/ajax/libs/waypoints/2.0.3/waypoints.min.js"></script>
+      <script src="assets/vendors/bootstrap/js/bootstrap.min.js"></script>
+      <script src="assets/vendors/jquery-ui/jquery-ui.min.js"></script>
+      <script src="assets/vendors/countdown-date-loop-counter/loopcounter.js"></script>
+      <script src="assets/js/jquery.counterup.js"></script>
+      <script src="assets/vendors/modal-video/jquery-modal-video.min.js"></script>
+      <script src="assets/vendors/masonry/masonry.pkgd.min.js"></script>
+      <script src="assets/vendors/lightbox/dist/js/lightbox.min.js"></script>
+      <script src="assets/vendors/slick/slick.min.js"></script>
+      <script src="assets/js/jquery.slicknav.js"></script>
+      <script src="assets/js/custom.min.js"></script>
+</body>
+</html>
